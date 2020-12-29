5cb7621f396bc4cd1e0dbcac6df97b417fc61885
lin <lin.wang@yunzhihui.com>
2020-12-29 17:12:09 +0800

    添加注释

--- redis-lock/src/main/java/com/example/redislock/RedislockApplication.java
+++ redis-lock/src/main/java/com/example/redislock/RedislockApplication.java
@@ -3,6 +3,9 @@ package com.example.redislock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * 查看test测试类
+ */
 @SpringBootApplication
 public class RedislockApplication {
 
